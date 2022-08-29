e07b443565e0e02bb4604a6a155f506f5a276d87
qinshiwei <985323014@qq.com>
2022-08-29 17:43:54 +0800

    project3

---
+++ project_3/cmake-build-debug/CMakeFiles/project_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_3.dir/main.cpp.o"
+  "project_3"
+  "project_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
