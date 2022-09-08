bd9efca4b792c040554655d9451e6833de3feaf4
qsw13619219831 <985323014@qq.com>
2022-09-08 11:43:16 +0800

    删除master里的文件

--- Project_4/Project_4/windos_test.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-	cout << "我来到台式机了" << endl;
-	return 0;
-}
--- project_2/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-        std::cout << "Hello, mac" << std::endl;
-    return 0;
-}
